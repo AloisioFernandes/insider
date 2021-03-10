ab3415327a838ad72293b727e19c8c6d85e1be7a
AloisioFernandes <aloisio.fernandes@outlook.com>
2021-03-10 20:57:26 -0300

    Estilo.

--- gerador/App.js
+++ gerador/App.js
@@ -1,14 +1,83 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native'
+
+import Slider from '@react-native-community/slider'
 
 export default function App() {
   return (
-    <View>
-      
+    <View style={styles.container}>
+      <Image 
+        source={require('./src/assets/logo.png')}
+        style={styles.logo}
+      />      
+
+      <Text style={styles.title}>12 Caracteres</Text>
+
+      <View style={[{paddingVertical: 9}, styles.area]}>
+        <Slider
+          style={{ height: 32 }}
+          minimumValue={5}
+          maximumValue={15}
+          minimumTrackTintColor="#F00"
+          maximumTrackTintColor="#000"
+        />
+      </View>
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Gerar Senha</Text>
+      </TouchableOpacity>
+
+      <View style={styles.area}>
+        <Text style={styles.password}>123123123123</Text>
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#F3F3FF'
+  },
   
+  logo: {
+    marginBottom: 60
+  },
+
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold'
+  },
+
+  area: {
+    marginTop: 15,
+    marginBottom: 15,
+    backgroundColor: '#FFF',
+    width: '80%',
+    borderRadius: 7
+  },
+
+  button: {
+    backgroundColor: '#FFA200',
+    width: '80%',
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 7,
+    marginBottom: 25
+  },
+
+  buttonText: {
+    fontSize: 20,
+    color: '#FFF',
+    fontWeight: 'bold'
+  },
+
+  password: {
+    padding: 10,
+    textAlign: 'center',
+    fontSize: 20
+  }
 })
